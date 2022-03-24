7c17839351964c2ca2c23643a061833153090858
Darshani_Kaushalya_Diwakara <darshani@gapstars.net>
2022-03-24 12:14:14 +0530

    feat: Add the Local DB Connection

--- server/database/database.js
+++ server/database/database.js
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
  */
 const connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        //MongoDB Cloud
+        // await mongoose.connect(process.env.MONGO_URI, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true
+        // });
+
+        //Local DB
+        await mongoose.connect(process.env.LOCAL_URI, { useNewUrlParser: true });
 
         console.log("mongoDB is connected")
     } catch (err) {
